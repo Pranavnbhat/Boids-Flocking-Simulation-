3f719fd4fba6e1d8afc068b118471b09389919d5
Pranavnbhat <pranavnbhat@gmail.com>
2026-05-12 09:16:46 +0530

    Added seperation baicss

--- main.py
+++ main.py
@@ -6,23 +6,48 @@ from pygame.math import Vector2 as vec
 
 
 class Boid(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,x,y):
         super().__init__()
-        
-        self.side = 15
-        self.h = math.sqrt(3)/2 * self.side
-        
-        self.image= pygame.Surface((self.side, int(self.h)), pygame.SRCALPHA)
-        
-        points = [
-            (self.side/2, 0),        # top
-            (0, self.h),             # bottom left
-            (self.side, self.h)           # bottom right
-        ]
+        self.side = 15        
+        self.h = math.sqrt(3)/2 * self.side        
+        self.image= pygame.Surface((self.side, int(self.h)), pygame.SRCALPHA) 
+        self.vec=pygame.vec(spawn_pos_x, spawn_pos_y)
 
-        pygame.draw.polygon(self.image, (255, 255, 255), points)
+        points = [        
+            (self.side/2, 0),        # top        
+            (0, self.h),             # bottom left        
+            (self.side, self.h)           # bottom right        
+        ]        
+        pygame.draw.polygon(self.image, (255, 255, 255), points)        
         self.rect = self.image.get_rect(center=(400, 300))
 
+
+    
+        
+        
+    def perception_radius(self):
+
+                
+    
+    def seperation(self):
+        for Boid in Boidgroup:
+            if Boid!= self:
+                distance=(self.vec - Boid.vec).length()
+                if distnace<perception_radius:
+                    if self.rect.centerx<Boid.rect.centerx:
+                        #move left 
+                    if self.rect.centerx>Boid.rect.centerx:  
+                        #move right 
+                    if self.rect.centery<Boid.rect.centery:
+                        #move down 
+                    if self.rect.centery>Boid.rect.centery:    
+                        #move up 
+        
+    def alignement(self):
+
+    def cohesion(self):
+    
+
         
         
         
@@ -38,25 +63,25 @@ class Boid(pygame.sprite.Sprite):
         
         
         
-        pygame.init()
-        screen = pygame.display.set_mode((800,600))
-        pygame.display.set_caption('Boids')
-        clock = pygame.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption('Boids')
+clock = pygame.time.Clock() 
+            
         
         
         
         
         
         
-        while True: 
-            screen.fill(206,255,255)
+while True: 
+    screen.fill((206,255,255))
+            
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:        
+            pygame.quit()        
+            exit()        
             
             
-            
-            
-            
-            
-            
-            
-            pygame.display.update()
-            clock.tick(60)
+    pygame.display.update()
+    clock.tick(60)                  
